763badb69712e9ae9c78f73e5256335328554f3f
MSaktiAlvissalim <msaktialvissalim@MSaktiAlvissalim-no-MacBook-Pro.local>
2014-12-04 13:44:02 +0900

    added legend

---
+++ NASAGIBS/GIBSLegendBuilder.h
@@ -0,0 +1,13 @@
+//
+//  GIBSLegendBuilder.h
+//  NASAGIBS
+//
+//  Created by MSaktiAlvissalim on 2014/12/03.
+//  Copyright (c) 2014年 Alvis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GIBSLegendBuilder : NSObject
+
+@end
---
+++ NASAGIBS/GIBSLegendBuilder.m
@@ -0,0 +1,13 @@
+//
+//  GIBSLegendBuilder.m
+//  NASAGIBS
+//
+//  Created by MSaktiAlvissalim on 2014/12/03.
+//  Copyright (c) 2014年 Alvis. All rights reserved.
+//
+
+#import "GIBSLegendBuilder.h"
+
+@implementation GIBSLegendBuilder
+
+@end
